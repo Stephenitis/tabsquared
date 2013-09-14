b9316e32a0d095c0be3ed1554b527b10b1656d59
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-09-14 16:51:35 -0700

    Rails boilerplate.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tabsquared::Application.config.secret_key_base = '6220b49a15d71fad42047157c2afaedf7a85a7f94a1134841dbaf9c2a1031bc7e7266a018e8b3e0620b8f06bb2eca2fed8cb9aae6b7e4b6803f1fc850d90fd0c'
