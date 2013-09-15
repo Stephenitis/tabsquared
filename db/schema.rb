8a3f1ac1adc36870ca4545db479b3ab5385805f4
Jared Smith <jaredsmithse@icloud.com>
2013-09-15 08:11:21 -0700

    Remove extrenneous sqlite file, add updated shema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130915040057) do
+ActiveRecord::Schema.define(version: 20130915150717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(version: 20130915040057) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "phone_number"
     t.integer  "uid"
     t.string   "oauth_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone_number"
   end
 
   create_table "venues", force: true do |t|
