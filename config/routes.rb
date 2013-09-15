12160ebc15f2918eae721bbcb299a042b1daccd5
Jared Smith <jaredsmithse@icloud.com>
2013-09-15 12:02:54 -0700

    Update config routes for new controller changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Tabsquared::Application.routes.draw do
   root 'welcome#index'
 
   resources :users, only: [:show, :edit, :update]
-  resources :sessions, only: [:create, :destroy]
+  resources :sessions, only: [:create]
+
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   get '/auth/foursquare/callback', to: 'sessions#create'
 
   post '/listener', to: 'command#listener'
 
   post '/test', to: 'command#test'
+
+  post '/verify', to: 'users#verify'
 
 end
