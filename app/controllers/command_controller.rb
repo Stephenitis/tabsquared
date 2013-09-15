92297fa096f276ac01349eb59c8bf2bf57f38fd1
Jared Smith <jaredsmithse@icloud.com>
2013-09-15 13:16:38 -0700

    Make dynamic checkin handling to allow any manager to get their feed

--- app/controllers/command_controller.rb
+++ app/controllers/command_controller.rb
@@ -2,7 +2,8 @@ class CommandController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 	def listener		
 		checkin = JSON.parse(URI.decode_www_form(request.raw_post, enc=Encoding::UTF_8)[0][1])
-		checkin[:phone] = [7039755863]
+		managers = Venue.find_by(venue_id: checkin['venue']['id']).users
+		checkin[:phone] = managers.map { |manager| manager.phone_number }
 		HTTParty.post("https://mq-aws-us-east-1.iron.io/1/projects/52342e7c245f5c0009000001/queues/tabsquaredqueue/messages/webhook?oauth=JyzomLPboE13Gni-d7VgfpoMPWw", body: checkin.to_json, options: { :headers => { 'ContentType' => 'application/json' } })
 		# response = HTTParty.post("http://requestb.in/13c4d4f1", body: checkin.to_json, options: { :headers => { 'ContentType' => 'application/json' } })
 		render nothing: true
