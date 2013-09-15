5e852bf93450d0fa4536a3946a187503fc3a09ae
Jared Smith <jaredsmithse@icloud.com>
2013-09-15 15:58:57 -0700

    Add more semantic parsing of data

--- app/controllers/command_controller.rb
+++ app/controllers/command_controller.rb
@@ -7,9 +7,10 @@ class CommandController < ApplicationController
 		p checkin
 		puts "*"*100
 		puts "*"*100
-		managers = Venue.find_by_venue_id(checkin['venue']['id'])
+		managers = Venue.where(venue_id: checkin['venue']['id']).first
 		managers = managers.users
 		checkin[:phone] = managers.map { |manager| manager.phone_number }
+
 		HTTParty.post("https://mq-aws-us-east-1.iron.io/1/projects/52342e7c245f5c0009000001/queues/tabsquaredqueue/messages/webhook?oauth=JyzomLPboE13Gni-d7VgfpoMPWw", body: checkin.to_json, options: { :headers => { 'ContentType' => 'application/json' } })
 		# response = HTTParty.post("http://requestb.in/13c4d4f1", body: checkin.to_json, options: { :headers => { 'ContentType' => 'application/json' } })
 		render nothing: true
