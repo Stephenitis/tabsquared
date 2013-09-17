3ca570f9e8d8c71d2d44b8ea9ba400f22b17f98f
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-09-16 17:26:03 -0700

    unneeded variable assignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,8 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :venues
   
   def self.find_or_create_from_auth_hash(auth_hash)
-    uid = auth_hash["uid"]
-
     where(uid: uid).first_or_initialize.tap do |user|
-      user.uid = uid
+      user.uid = auth_hash["uid"]
       user.first_name = auth_hash["info"]["first_name"]
       user.last_name = auth_hash["info"]["last_name"]
       user.oauth_token = auth_hash["credentials"]["token"]
