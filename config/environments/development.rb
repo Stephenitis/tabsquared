707b6c62661ef543a753e22d197aee19ce3fe668
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-09-16 21:41:31 -0700

    Fix no log output in development with foreman.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ Tabsquared::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  $stdout.sync = true
 end
