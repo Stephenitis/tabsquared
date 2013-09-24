3acd4f58b081f5d408c79af85fdedc4c37b35e51
Stephen Nguyen <nguyenhstephen@gmail.com>
2013-09-23 22:17:53 -0700

    I extracted our worker/webhook out into the ENV unsure if it works I also extracted our rails session cookie token out to the ENV so beware

--- app/controllers/command_controller.rb
+++ app/controllers/command_controller.rb
@@ -17,7 +17,7 @@ class CommandController < ApplicationController
 private
 
   def send_to_worker(checkin)
-    HTTParty.post("https://mq-aws-us-east-1.iron.io/1/projects/52342e7c245f5c0009000001/queues/tabsquaredqueue/messages/webhook?oauth=JyzomLPboE13Gni-d7VgfpoMPWw", body: checkin.to_json, options: { :headers => { 'ContentType' => 'application/json' } })
+    HTTParty.post(ENV["WORKER_WEBHOOK_URL"], body: checkin.to_json, options: { :headers => { 'ContentType' => 'application/json' } })
   end
 
   def find_managers(checkin)
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tabsquared::Application.config.secret_key_base = '6220b49a15d71fad42047157c2afaedf7a85a7f94a1134841dbaf9c2a1031bc7e7266a018e8b3e0620b8f06bb2eca2fed8cb9aae6b7e4b6803f1fc850d90fd0c'
+Tabsquared::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
